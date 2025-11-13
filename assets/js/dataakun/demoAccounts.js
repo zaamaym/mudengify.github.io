7e8446afdebc429b68fa2074c8eb2894048e6f00
zaamaym <155287707+zaamaym@users.noreply.github.com>
2025-11-13 21:38:56 +0700

    Add files via upload

--- assets/js/dataakun/demoAccounts.js
+++ assets/js/dataakun/demoAccounts.js
@@ -1,1 +1,6 @@
-
+// === DATA AKUN DEMO ===
+const DEMO_ACCOUNTS = [
+  { username: "siswa123", password: "mudengify" },
+  { username: "guru001", password: "demo123" },
+  { username: "admin", password: "12345" },
+];
