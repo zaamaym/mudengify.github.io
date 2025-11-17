9abe4c6fec8469f07b0c065640d7007c35e90b05
zaamaym <155287707+zaamaym@users.noreply.github.com>
2025-11-17 07:41:27 +0700

    Add files via upload

---
+++ assets/js/dataakun/accounts.js
@@ -0,0 +1,23 @@
+// === DATA AKUN === accounts.js
+const ACCOUNTS_DATA = [
+  {
+    username: "siswa123",
+    password: "mudengify",
+    role: "user"
+  },
+  {
+    username: "guru001",
+    password: "guruterbaik",
+    role: "admin"
+  },
+  { 
+    username: "murid123",
+    password: "mudengify",
+    role: "user"
+  },
+  { 
+    username: "admin",
+    password: "12345",
+    role: "admin"
+  },
+];
