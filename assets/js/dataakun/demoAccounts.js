acccf98b34b5a6673f7af5d408c7fb45f844efad
zaamaym <155287707+zaamaym@users.noreply.github.com>
2025-11-17 07:41:44 +0700

    Delete assets/js/dataakun/demoAccounts.js

--- assets/js/dataakun/demoAccounts.js
+++
@@ -1,6 +0,0 @@
-// === DATA AKUN DEMO ===
-const DEMO_ACCOUNTS = [
-  { username: "siswa123", password: "mudengify" },
-  { username: "guru001", password: "demo123" },
-  { username: "admin", password: "12345" },
-];
